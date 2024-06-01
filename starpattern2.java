7b8730aa4c40ad7661efc55cf67f8cc13a1f12c1
Shreyas Mishra <91563671+shreyaspvt@users.noreply.github.com>
2024-06-01 22:05:35 +0530

    Add files via upload

---
+++ starpattern2.java
@@ -0,0 +1,22 @@
+/*
+* * * *
+* * *
+* *
+*
+Key point is "n - line + 1"
+ */
+import java.util.*;
+public class starpattern2
+{
+    public static void main (String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=6;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n-i+1; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
