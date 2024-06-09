048a199e5239d9f16f8f272f50f3c0576e5db316
Shreyas Mishra <91563671+shreyaspvt@users.noreply.github.com>
2024-06-09 17:38:24 +0530

    Add files via upload

---
+++ HollowRhombus.java
@@ -0,0 +1,33 @@
+/* To print the following pattern
+    *****
+   *   *
+  *   *
+ *   *
+*   *
+*/
+import java.util.*;
+public class HollowRhombus
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int n = sc.nextInt();
+        for( int i = 1; i <= n; i++)
+        {
+            // spaces
+            for ( int j = 1; j <= (n-i); j++)
+                System.out.print(" ");
+            // stars
+            for( int j = 1; j <= n; j++)
+            {
+                if ( i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+
+            }
+            System.out.println();
+        }
+    }
+}
